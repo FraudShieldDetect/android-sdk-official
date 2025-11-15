f8e73d2639ea1e4bc28fdd408dfe539a548fcb43
YehyaTamimi <tamimiyehya@gmail.com>
2025-11-15 14:34:51 +0200

    add debug info collector

---
+++ protosdk/src/main/java/com/protosdk/sdk/fingerprint/collectors/DebugInfoCollector.kt
@@ -0,0 +1,59 @@
+package com.protosdk.sdk.fingerprint.collectors
+
+import android.content.Context
+import android.os.Debug
+import com.protosdk.sdk.fingerprint.interfaces.BaseCollector
+import com.protosdk.sdk.fingerprint.utils.SecurityCollectorUtils
+import org.json.JSONObject
+
+class DebugInfoCollector : BaseCollector() {
+
+  override suspend fun collect(context: Context): JSONObject = safeCollect {
+    JSONObject().apply {
+      put(
+        "adbEnabled",
+        SecurityCollectorUtils.readGlobalSetting(
+          context,
+          android.provider.Settings.Global.ADB_ENABLED,
+        ),
+      )
+      put(
+        "developmentSettingsEnabled",
+        SecurityCollectorUtils.readGlobalSetting(
+          context,
+          android.provider.Settings.Global.DEVELOPMENT_SETTINGS_ENABLED,
+        ),
+      )
+      put(
+        "waitForDebugger",
+        SecurityCollectorUtils.readGlobalSetting(
+          context,
+          android.provider.Settings.Global.WAIT_FOR_DEBUGGER,
+        ),
+      )
+
+      put("isDebuggerConnected", Debug.isDebuggerConnected())
+      put("isDebuggerWaiting", Debug.waitingForDebugger())
+
+      // val memoryInfo = Debug.MemoryInfo().also { Debug.getMemoryInfo(it) }
+      // put(
+      //         "debugMemoryInfo",
+      //         JSONObject().apply {
+      //           put("totalPrivateDirty", memoryInfo.totalPrivateDirty)
+      //           put("totalPrivateClean", memoryInfo.totalPrivateClean)
+      //           put("totalSharedDirty", memoryInfo.totalSharedDirty)
+      //           put("totalSharedClean", memoryInfo.totalSharedClean)
+      //           put("totalPss", memoryInfo.totalPss)
+      //           put("totalSwappablePss", memoryInfo.totalSwappablePss)
+      //         },
+      // )
+      // put("nativeHeapAllocatedSize", Debug.getNativeHeapAllocatedSize())
+      // put("nativeHeapFreeSize", Debug.getNativeHeapFreeSize())
+      // put("nativeHeapSize", Debug.getNativeHeapSize())
+    }
+  }
+
+  override fun getCollectorName(): String = "DebugInfoCollector"
+
+  override fun getRequiredPermissions(): List<String> = emptyList()
+}
